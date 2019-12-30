62bc96f41f60e427d946903af2ecdbb3ea5d2e7b
Olivia Coffey <ocoffey@protonmail.com>
2019-12-30 12:31:05 -0500

    stashing for future work

--- K-DTree/KDTree.rb
+++ K-DTree/KDTree.rb
@@ -2,17 +2,19 @@
 class KDNode
     # takes an array of dimensional data,
     # the depth of the node,
-    # and possible data for the node
+    # and possible data for the node (for k-nn)
     # left and right children set to nil
     # also keeps track of the number of children
     # for nearest neighbor searches
-    def initialize(dimensions, depth, data)
+    # and the parent node, for ease of traversal
+    def initialize(dimensions, depth, data, parent = nil)
         @dimensions = dimensions
         @depth = depth
         @data = data
         @left = nil
         @right = nil
         @num_children = 0
+        @parent = parent
     end
 
     # returns the depth of the node
@@ -46,13 +48,44 @@ class KDTree < KDNode
         current[current.get_depth % @num_dimensions]
     end
 
+    # finds the leaf closest to given dimensional data
+    # used in insert and nn search
+    def find_leaf(current, dimensions, data)
+        # base case
+        # if we hit nil, we know that the previous node is a leaf
+        if current == nil
+            return true
+        # otherwise, traverse down left and right
+        # until we find the leaf
+        else
+            # if the current node is greater or equal
+            # to the current node, then traverse right
+            if self.get_node_data(current) <= dimensions[current.depth % @num_dimensions]
+                leaf = find_leaf(current.right, dimensions, data)
+                if leaf == true
+                    return current
+                end
+            # otherwise, traverse left
+            else
+                leaf = find_leaf(current.right, dimensions, data)
+                if leaf == true
+                    return current
+                end
+            end
+            # if leaf wasn't 'true',
+            # it means that it's a node address
+            # that we should return
+            return leaf
+        end
+    end
+
     # initial inserting method
     # inserts at 'root' for an empty tree,
     # calls a recursive helper method if
     # root has a node
     def insert(dimensions, data = nil)
         # empty tree, make the first node the root
-        if @root = nil
+        if @root == nil
             @root = KDNode.new(dimensions, 0, data)
             # set the number of dimensions used as tree data
             # for modulus bounding
@@ -66,76 +99,49 @@ class KDTree < KDNode
         @num_nodes += 1
     end
 
-    # insert recursive method
-    # compares the appropriate dimensional data passed
-    # to the dimensional data of 'current'
-    # recursively inserts appropriately 
+    # insert helper method
+    # calls find_leaf to know where to place the node
+    # does a comparison for which child to place node at
+    # then places it there
     def insert(current, dimensions, data)
-        # base case
-        # if 'current' is nil,
-        # let the previous frameknow to insert here
-        if current == nil
-            return true
-        end
-        # otherwise compare the appropriate dimension
-        # of the current node to soon-to-be new node
-
-        # if the current node data is less or equal to the passed node,
-        if self.get_node_data(current) <= dimensions[current.get_depth % @num_dimensions]
-            # go right
-            insert_here = insert(current.right, dimensions, data)
-            # if 'right' is nil, insert the node there
-            if insert_here
-                current.right = KDNode.new(dimensions, current.get_depth + 1, data)
-                current.increment_children
-            end
-        # otherwise the node is less
+        leaf = find_leaf(current, dimensions, data)
+        # if node to make has greater or equal data than the leaf
+        if self.get_node_data(leaf) <= dimensions[leaf.depth % @num_dimensions]
+            # place to the right
+            leaf.right = KDNode.new(dimensions, leaf.depth + 1, data, leaf)
+        # otherwise place the node to the left
         else
-            # go left
-            insert_here = insert(current.left, dimensions, data)
-            # if 'left' is nil
-            if insert_here
-                current.left = KDNode.new(dimensions, current.get_depth + 1, data)
-                current.increment_children
-            end
+            leaf.left = KDNode.new(dimensions, leaf.depth + 1, data, leaf)
         end
-        # to let any previous frames know not to insert
-        # based off of 'current'
-        return false
     end
 
     # for fun, keep track of the total number of nodes
     def get_size
         @num_nodes
     end
-
-    def k_traverse(k, current)
-        # less than required neighbors at the current node
-        if current.num_children < k
-            return true
-        # correct amount of neighbors at the current node
-        elsif current.num_children == k
-            return current
-        # need more neighbors
-        else
-            find_neighbors = self.k_traverse(k, )
-        end
-    end
     
+    def bubble_up(current, dimensions, data)
+        
+    end
+
     # returns the 'k' nearest neighbors
     # of given dimensional data
-    def nearest_neighbors(k, dimensions)
+    def nearest_neighbors(k, dimensions, data)
         # if the passed array isn't the same length as
         # the number of dimensions recorded
         if dimensions.length != @num_dimensions
             # print to stderror to let the user know to input
             # appropriate data
             # no error raised because we don't want
-            # to halt the program
+            # to halt the program/destroy the tree
             STDERR.puts "dimensional data inappropriate length"
             return
         else
-
+            # otherwise, find the leaf nearest the point
+            leaf = find_leaf(@root, dimensions, data)
+            
+            
+            # look up more in the algorithm
         end
 
     end
